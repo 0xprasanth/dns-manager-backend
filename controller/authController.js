4f9cb41e5121b1928af112c3d31b42f7c1b82874
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-05-09 02:13:28 +0530

    fix(db): signup and login routes with DB connection

--- controller/authController.js
+++ controller/authController.js
@@ -1,8 +1,7 @@
 // const { default: mongoose } = require("mongoose");
 // const userModel = require("../model/Users");
 
-const { prisma } = require('../db');
-
+const { prisma } = require("../db");
 
 const {
   signToken,
@@ -11,82 +10,110 @@ const {
 } = require("../utils/validate");
 
 exports.createSendToken = (user, res) => {
-    const token = signToken(user.id);
+  const token = signToken(user.id);
 
-    const CookieOptions = {
-        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-    }
+  const CookieOptions = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
 
-    // update for PROD server
-    if (process.env.NODE_ENV==="production") {
-        CookieOptions.secure = true;
-    }
-    
-    res.cookie("jwt", token, CookieOptions);
+  // update for PROD server
+  if (process.env.NODE_ENV === "production") {
+    CookieOptions.secure = true;
+  }
 
-    // update the cookie 
-    // return the token
-    return token;
+  res.cookie("jwt", token, CookieOptions);
+
+  // update the cookie
+  // return the token
+  return token;
 };
 
 exports.signup = async (req, res) => {
-    const {username, email, password} = req.body
+  const { username, email, password } = req.body;
 
-    console.log(req.body);
-    // Check for existing USer
-    const isUserExist = await prisma.user.findUnique({
-        where: {email: email},
+  // Check for existing USer
+  const isUserExist = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (isUserExist) {
+    return res.status(400).json({
+      meesage: `User with ${email} already exist`,
+    });
+  }
+
+  // if it is new user, hash password and insert into DB
+  const cryptPassword = await hashedPassword(password);
+
+  // Create user and push into DB
+  try {
+    const createUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: cryptPassword,
+        username: username,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+      },
     });
 
-    if(isUserExist){
-        return res.status(400).json({
-            meesage: `User with ${email} already exist`
-        })
-    }
+    // generate access token
+    const accesstoken = this.createSendToken(createUser, res);
 
-    // if it is new user, hash password and insert into DB
-    const cryptPassword = await hashedPassword(password);
-
-    // Create user and push into DB
-
-    try{
-        const user = await prisma.user.create({
-            data: {
-              email: email,
-              password: cryptPassword,
-              username: username
-            },
-            select: {
-              id: true,
-              email: true,
-              username: true
-            },
-          });
-
-        // generate access token
-        const accesstoken = this.createSendToken(user, res);
-
-        // if success
-        res.status(201).json({
-            message: "successed",
-            data: user,
-            accesstoken
-        });
-
-    } catch(error){
-        res.status(500).json({
-            message: `Error: ${error.message}`
-        })
-    }
+    // if success
+    res.status(201).json({
+      message: "successed",
+      data: createUser,
+      accesstoken,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error: ${error.message}`,
+    });
+  }
 };
 
 exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ status: 400, message: "Please provide a email and password" });
+  }
 
-    console.log(req.body
-    );
-  res.status(200).json({
-    message: "Hello from /login",
-    body: req.body  
+  const user = await prisma.user.findUnique({
+    where: { email: email },
   });
+
+  // if user not found
+  if (!user) {
+    return res.status(401).json({
+        status: 401,
+        message: "Email ID does not exist. Please check again"
+    });
+  }
+  
+  //validate passwd
+  const passwordMatch = await validatePassword(password, user.password);
+
+  if (!passwordMatch) {
+    return res
+      .status(401)
+      .json({ status: 401, message: "Invalid password. Check again!" });
+  }
+
+  const accesstoken = this.createSendToken(user, res);
+  //eliminate the password field!!
+  user.password = undefined;
+
+  res.status(200).json({
+    message: "success",
+    accesstoken,
+    data: user,
+  });
+
 };

3dd825980cc32e112c6be908b7355da783362266
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-05-11 02:28:14 +0530

    add headers for OPTIONS

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -11,4 +11,17 @@ router
     .post("/signup", authController.signup)
     .post("/login", authController.login)
 
+router.options('/login', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  })
+  .options('/signup', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+  });
+
 module.exports = router;
